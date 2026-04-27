74fa0b534bb0b741ef9e211f04d1e75c8efdefe2
iyricc-8 <rustambaev09@gmail.com>
2026-04-27 16:44:55 +0500

    fix: enable superadmin institution deletion

--- backend/apps/superadmin/views.py
+++ backend/apps/superadmin/views.py
@@ -20,6 +20,7 @@ class SuperadminInstitutionViewSet(
     mixins.ListModelMixin,
     mixins.CreateModelMixin,
     mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
     viewsets.GenericViewSet,
 ):
     serializer_class = InstitutionSerializer
@@ -32,6 +33,15 @@ class SuperadminInstitutionViewSet(
     def retrieve(self, request, *args, **kwargs):
         with schema_context("public"):
             return super().retrieve(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        with schema_context("public"):
+            institution = self.get_object()
+            try:
+                institution.delete(force_drop=True)
+            except TypeError:
+                institution.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
     def get_queryset(self):
         queryset = Institution.objects.annotate(
