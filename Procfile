2e92c6ec86ab114402af5eb3d84a47486d9bc167
iyricc-8 <rustambaev09@gmail.com>
2026-05-16 23:41:37 +0500

    feat: run celery worker and beat in same container via start.sh

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: sh -c 'cd backend && python manage.py migrate_schemas --shared && python manage.py collectstatic --noinput && daphne -b 0.0.0.0 -p $PORT config.asgi:application'
+worker: sh -c 'cd backend && celery -A config worker -l warning -c 2'
+beat: sh -c 'cd backend && celery -A config beat -l warning --scheduler django_celery_beat.schedulers:DatabaseScheduler'
---
+++ docker/start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+cd /app/backend
+
+echo "Running migrations..."
+python manage.py migrate_schemas --shared
+
+echo "Collecting static..."
+python manage.py collectstatic --noinput
+
+echo "Starting Celery worker in background..."
+celery -A config worker -l warning -c 2 &
+WORKER_PID=$!
+
+echo "Starting Celery beat in background..."
+celery -A config beat -l warning \
+  --scheduler django_celery_beat.schedulers:DatabaseScheduler &
+BEAT_PID=$!
+
+echo "Starting Daphne..."
+exec daphne -b 0.0.0.0 -p $PORT config.asgi:application
